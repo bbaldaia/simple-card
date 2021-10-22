df838fe31d6bdf8e29bb834695557c02a1dc9439
bbaldaia <brunobfreire98@gmail.com>
2021-10-21 21:06:01 -0300

    Consumindo API do Github com Javascript

--- scripts.js
+++ scripts.js
@@ -13,4 +13,17 @@ function changeSocialMediaLinks() {
     }
 }
 
-changeSocialMediaLinks()
+function getGithubData() {
+    const url = `https://api.github.com/users/${links.github}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const githubName = data.name
+        const githubBio = data.bio
+        document.querySelector('#username').textContent = githubName
+        document.querySelector('#bio').textContent = githubBio
+    })  
+}
+
+getGithubData()
+// changeSocialMediaLinks()
